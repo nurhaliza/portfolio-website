1aa22a0117b438f82d650d2460b27cca15ee0d72
nurhaliza <nurhaliza@csu.fullerton.edu>
2022-05-15 23:30:22 -0700

    major changes to all pages

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+/* --- active section --- */
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("link-item") && e.target.hash !== "") {
+        navToggler.classList.add("hide");
+        if(e.target.classList.contains("nav-item")) {
+            toggleNavbar();
+        } else {
+            hideSection();
+            document.body.classList.add("hide-scrolling");
+        } 
+        setTimeout(() => {
+            document.querySelector("section.active").classList.remove("active", "fade-out");
+            document.querySelector(e.target.hash).classList.add("active");
+            window.scrollTo(0,0);
+            document.body.classList.remove("hide-scrolling");
+            navToggler.classList.remove("hide");
+        }, 500);
+    }
+})
 /* ---  about me - tabs (education/experience) --- */
 const tabsContainer = document.querySelector(".about-tabs"),
 aboutSection = document.querySelector(".about-section");
@@ -16,9 +35,31 @@ tabsContainer.addEventListener("click", (e) => {
 document.addEventListener("click", (e) => {
     if(e.target.classList.contains("view-project-btn")) {
         togglePortfolioPopup();
+        portfolioItemDetails();
     }
 })
 
+// hides popup when clicking outside of the popup
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("pp-inner")) {
+        togglePortfolioPopup();
+    }
+});
+
 function togglePortfolioPopup() {
     document.querySelector(".portfolio-popup").classList.toggle("open");
+    document.body.classList.toggle("hide scrolling");
+    document.querySelector(".main").classList.toggle("fade-out");
+}
+document.querySelector(".pp-close").addEventListener("click", togglePortfolioPopup);
+
+function portfolioItemDetails(portfolioItem) {
+    document.querySelector(".pp-thumbnail img").src = 
+    portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
+
+    document.querySelector(".pp-header h3").innerHTML = 
+    portfolioItem.querySelector(".portfolio-item-title").innerHTML;
+
+    document.querySelector(".pp-body").innerHTML = 
+    portfolioItem.querySelector(".portfolio-item-details").innerHTML;
 }
